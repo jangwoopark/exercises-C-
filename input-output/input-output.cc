1839d91f5be2f2b0d4445a4e76cb1f24c80cd865
Jang Woo Park <jangpark@g.ucla.edu>
2019-04-01 16:37:17 -0700

    Create input-output.cc
    prog

---
+++ input-output/input-output.cc
@@ -0,0 +1,17 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    int Number_One, Number_Two, Number_Three;
+
+    cin >> Number_One >> Number_Two >> Number_Three;
+
+    cout << Number_One + Number_Two + Number_Three << endl;
+    return 0;
+}
